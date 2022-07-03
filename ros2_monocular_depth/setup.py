fc90cc75af60790906b4f0614f363513f27fc9ea
Rhys Williams <rhysdgwilliams@gmail.com>
2022-07-03 22:28:24 +0100

    midas module fix

--- ros2_monocular_depth/setup.py
+++ ros2_monocular_depth/setup.py
@@ -5,7 +5,7 @@ package_name = 'ros2_monocular_depth'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=[package_name,package_name + '/MiDaS'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
